aad03cfb8f8ad90e2fcaea84e3d9c3018f037759
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-05-08 22:43:35 -0400

    added main.py and vehicles.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from vehicles import Car
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    car = Car()
+    while True:
+        action = input("What should I do? [A]ccelerate, [B]rake, "
+                       "show [O]dometer, or show average [S]peed?\n").upper()
+        if action not in "ABOS" or len(action) != 1:
+            print("I don't know how to do that")
+            continue
+        if action == 'A':
+            car.accelerate()
+        elif action == 'B':
+            car.brake()
+        elif action == 'O':
+            print("The car has driven {} kilometers".format(car.odometer))
+        elif action == 'S':
+            print("The car's average speed was {} kph".format(car.average_speed()))
+        car.step()
+        car.say_state()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
